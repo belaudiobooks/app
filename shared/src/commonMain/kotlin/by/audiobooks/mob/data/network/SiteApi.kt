23cdb1fae8937818f9a44748ac83d909106e5f4a
Aliaksandr Parfianiuk <frombrest@gmail.com>
2024-09-09 20:18:12 -0700

    Use GCS datasource (#13)

--- shared/src/commonMain/kotlin/by/audiobooks/mob/data/network/SiteApi.kt
+++ shared/src/commonMain/kotlin/by/audiobooks/mob/data/network/SiteApi.kt
@@ -9,6 +9,7 @@ import io.ktor.client.plugins.compression.ContentEncoding
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.HttpSendPipeline
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
@@ -20,9 +21,11 @@ import kotlin.random.Random
 class SiteApi {
 
     companion object {
+        private const val USE_RANDOM_ADDRESS = false
         private const val HOST = "audiobooksbysite.ew.r.appspot.com"
         private const val USER_AGENT = "audiobooks.by mob app"
         private const val CHAR_POOL = "abcdefghijklmnopqrstuvwxyz0123456789"
+        private const val STATIC_ADDRESS = "https://storage.googleapis.com/books_media/tmp_data.json"
         val DESERIALIZATION_SETTINGS = Json {
             // https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/json.md#coercing-input-values
             coerceInputValues = true
@@ -45,16 +48,30 @@ class SiteApi {
             exponentialDelay()
         }
     }.also {
-        it.sendPipeline.intercept(HttpSendPipeline.State) {
-            context.headers.remove(HttpHeaders.Host)
-            context.headers.append(HttpHeaders.Host, HOST)
+        if (USE_RANDOM_ADDRESS) {
+            it.sendPipeline.intercept(HttpSendPipeline.State) {
+                context.headers.remove(HttpHeaders.Host)
+                context.headers.append(HttpHeaders.Host, HOST)
+            }
         }
     }
 
     suspend fun downloadData(): BackendDataSnapshot =
         withContext(Dispatchers.IO) {
-            httpClient.get(getRandomAddress()).body()
+            val resp = httpClient.get(getAddress()) {
+                header("Accept", "application/json")
+            }
+            println(resp)
+            return@withContext resp.body()
         }
+
+    private fun getAddress(): String {
+        return if (USE_RANDOM_ADDRESS) {
+            getRandomAddress()
+        } else {
+            STATIC_ADDRESS
+        }
+    }
 
     private fun getRandomAddress(): String {
         val randomDomain = (1..Random.nextInt(1,45))
