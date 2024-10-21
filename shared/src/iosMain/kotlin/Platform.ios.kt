6625c6c1d542653a0e8d15a64af97d1f4c7cb270
Aliaksandr Parfianiuk <frombrest@gmail.com>
2024-10-20 19:49:48 -0700

    getRepository fabric method

--- shared/src/iosMain/kotlin/Platform.ios.kt
+++ shared/src/iosMain/kotlin/Platform.ios.kt
@@ -1,3 +1,9 @@
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import by.audiobooks.mob.data.Repository
+import by.audiobooks.mob.data.RepositoryImpl
+import by.audiobooks.mob.data.db.AudiobooksByDB
+import by.audiobooks.mob.data.db.DatabaseHelper
+import by.audiobooks.mob.data.network.SiteApi
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
@@ -5,3 +11,10 @@ class IOSPlatform: Platform {
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+fun getRepository(): Repository {
+    return RepositoryImpl(
+        dbHelper = DatabaseHelper(NativeSqliteDriver(AudiobooksByDB.Schema, "audiobooks-by.db")),
+        siteApi = SiteApi()
+    )
+}
