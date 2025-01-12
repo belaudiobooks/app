9a6f320c5a213830b87044492c95604c09296022
spryby <sergeyprybysh@gmail.com>
2025-01-11 20:49:06 -0800

    wp

--- iosApp/iosApp/CommonUI/BookCoverImageView.swift
+++ iosApp/iosApp/CommonUI/BookCoverImageView.swift
@@ -10,14 +10,14 @@ import SwiftUI
 
 struct BookCoverImageView: View {
   let imageURL: String
+  let fallbackState: FallbackView.State
   
   var body: some View {
     VStack(alignment: .leading) {
       AsyncImage(url: URL(string: imageURL)) { phase in
         switch phase {
         case .failure:
-          Image(systemName: "photo")
-            .font(.largeTitle)
+          FallbackView(state: fallbackState)
         case .success(let image):
           image
             .resizable()
@@ -30,7 +30,62 @@ struct BookCoverImageView: View {
   }
 }
 
+struct FallbackView: View {
+  struct State {
+    let authorName: String
+    let title: String
+    let color: Color
+  }
+  
+  let state: State
+  
+  var body: some View {
+    ZStack {
+      Color.purple
+        .ignoresSafeArea()
+        .overlay(
+          LinearGradient(colors: [.purple, .pink], startPoint: .top, endPoint: .bottom)
+            .opacity(0.3)
+        )
+        .background(
+          Rectangle()
+            .fill(Color.white.opacity(0.05))
+            .blur(radius: 20)
+        )
+      
+      VStack(spacing: 20) {
+        Text(state.authorName)
+          .font(._title3)
+          .foregroundColor(.white)
+        
+        Rectangle()
+          .fill(Color.purple)
+          .frame(height: 4)
+          .frame(maxWidth: 150)
+        
+        Text(state.title)
+          .font(._title3)
+          .fontWeight(.bold)
+          .foregroundColor(.white)
+      }
+    }
+  }
+}
 
 #Preview {
-  BookCoverImageView(imageURL: "https://storage.googleapis.com/books_media/covers/liubits-noch-prava-patsukou.jpg")
+  BookCoverImageView(
+    imageURL: "https://storage.googleapis.com/books_media/covers/liubits-noch-prava-patsukou.jpg",
+    fallbackState: .init(
+      authorName: "Іван Свістулькін",
+      title: "Доўгая дарога дадому",
+      color: .green))
+}
+
+#Preview {
+  BookCoverImageView(
+    imageURL: "https://storage.googleapis.com/books_media/covers/liubits-noch-prava.jpg",
+    fallbackState: .init(
+      authorName: "Іван Свістулькін",
+      title: "Доўгая дарога дадому",
+      color: .green))
 }
